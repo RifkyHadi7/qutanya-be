67c3eb08031d9f5940aae4a7dc47f2f2e287f32b
RifkyHadi7 <rifkyhadi7@gmail.com>
2024-09-13 04:22:08 +0700

    artikel by id

--- routers/artikel.router.js
+++ routers/artikel.router.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const controller = require("../controllers/artikel.controller");
 
 router.get("/", controller.getAllArtikel)
+router.get("/:id", controller.getAllArtikelbyId)
 
 module.exports = router
 
