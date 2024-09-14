ad7ea394916cb4596880bd8ec159d92d62e16bbb
NovanM <azizinovanm@gmail.com>
2024-09-14 20:32:18 +0700

    fix

--- index.js
+++ index.js
@@ -9,13 +9,9 @@ const saldoRouter = require("./routers/saldo.router");
 const kategoriRouter = require("./routers/kategori.router");
 const app = express()
 const url = require('url');
-const port = 3001
+const port = 3000
 
-app.use(cors({
-  origin: '*', // Mengizinkan semua origin
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ["*"]
-}))
+app.use(cors("*"))
 app.use(express.json());
 
 app.get('/', async(req, res) => {
